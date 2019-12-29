3e178598716dc37b4e0ab4566a8a5e3e60521b03
Marcin Ratajczak <maratajczak@gmail.com>
2019-12-29 16:21:18 +0100

    create component

---
+++ src/data.js
@@ -0,0 +1,142 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "iPhone 7",
+    img: "img/product-1.png",
+    price: 10,
+    info: `- 4.7 Inch Retina HD display
+    - IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes)
+    - 12MP camera and 4K video and 7MP FaceTime HD camera with Retina Flash
+    - Touch ID for secure authentication and Apple Pay
+    - A10 Fusion chip
+    - iOS 12 with Screen Time, Group FaceTime, and even faster performance`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "iPhone 8",
+    img: "img/product-2.png",
+    price: 16,
+    info: `- 4.7-inch Retina HD display with True Tone
+    - IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes)
+    - 12MP camera with OIS and 4K video
+    - 7MP FaceTime HD camera with Retina Flash
+    - Touch ID for secure authentication and Apple Pay
+    - A11 Bionic with Neural Engine
+    - Wireless charging — works with Qi chargers
+    - iOS 12 with Screen Time, Group FaceTime, and even faster performance`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "iPhone X",
+    img: "img/product-3.png",
+    price: 25,
+    info: `- 5.8 Inch Super Retina display (OLED) with HDR
+    - IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes)
+    - 12MP dual cameras with dual OIS and 7MP TrueDepth front camera — Portrait mode and Portrait Lighting
+    - Face ID for secure authentication and Apple Pay
+    - A11 Bionic with Neural Engine
+    - Wireless charging — works with Qi chargers`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "iPhone XR",
+    img: "img/product-4.png",
+    price: 18,
+    info: `- 6.1-inch Liquid Retina display (LCD)
+    - IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes)
+    - 12MP camera with OIS and 7MP TrueDepth front camera — Portrait mode, Portrait Lighting, Depth Control and Smart HDR
+    - Face ID for secure authentication and Apple Pay
+    - A12 Bionic with next-generation Neural Engine
+    - Wireless charging — works with Qi chargers
+    - iOS 12 with Memoji, Screen Time, Siri Shortcuts and Group FaceTime`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "iPhone XS",
+    img: "img/product-5.png",
+    price: 30,
+    info: `- 5.8-inch Super Retina display (OLED) with HDR
+    - IP68 dust and water resistant (maximum depth of 2 metres up to 30 minutes)
+    - 12MP dual cameras with dual OIS and 7MP TrueDepth front camera — Portrait mode, Portrait Lighting, Depth Control and Smart HDR
+    - Face ID for secure authentication and Apple Pay
+    - A12 Bionic with next-generation Neural Engine
+    - Wireless charging — works with Qi chargers
+    - iOS 12 with Memoji, Screen Time, Siri Shortcuts and Group FaceTime`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "iPhone 11",
+    img: "img/product-6.png",
+    price: 39,
+    info: `- 6.1-inch Liquid Retina HD LCD display
+    - Water and dust resistant (2 metres for up to 30 minutes, IP68)
+    - Dual-camera system with 12MP Ultra Wide and Wide cameras; Night mode, Portrait mode, and 4K video up to 60fps
+    - 12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo
+    - Face ID for secure authentication and Apple Pay
+    - A13 Bionic chip with third-generation Neural Engine
+    - Fast-charge capable
+    - Wireless charging`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "iPhone 11 Pro",
+    img: "img/product-7.png",
+    price: 59,
+    info: `- 5.8-inch Super Retina XDR OLED display
+    - Water and dust resistant (4 metres for up to 30 minutes, IP68)
+    - Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps
+    - 12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo
+    - Face ID for secure authentication and Apple Pay
+    - A13 Bionic chip with third-generation Neural Engine
+    - Fast charge with 18W adapter included
+    - Wireless charging`,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    info:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed lacus nunc. Nulla mollis rutrum metus efficitur semper. Vestibulum vitae velit neque. Etiam risus purus, ultricies vitae congue placerat, efficitur faucibus mi. Donec gravida nisl et risus dictum mollis. Pellentesque sollicitudin convallis dui. In cursus bibendum rutrum. Proin ligula massa, consectetur non vehicula ut, egestas vel arcu. Mauris eu dui ut ex ornare scelerisque.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "iPhone 7",
+  img: "img/product-1.png",
+  price: 10,
+  info: `- 4.7 Inch Retina HD display
+    - IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes)
+    - 12MP camera and 4K video and 7MP FaceTime HD camera with Retina Flash
+    - Touch ID for secure authentication and Apple Pay
+    - A10 Fusion chip
+    - iOS 12 with Screen Time, Group FaceTime, and even faster performance`,
+  inCart: false,
+  count: 0,
+  total: 0
+};
