a84a61d9684918f069d35dff8b9bb229318f90d2
Marcin Ratajczak <maratajczak@gmail.com>
2020-01-12 15:25:51 +0100

    add cart component

--- src/context.js
+++ src/context.js
@@ -9,7 +9,8 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -67,6 +68,22 @@ class ProductProvider extends Component {
       return { modalOpen: false };
     });
   };
+
+  increment = id => {
+    console.log("increment");
+  };
+
+  decrement = id => {
+    console.log("decrement");
+  };
+
+  removeItem = id => {
+    console.log("remove item");
+  };
+
+  clearCart = () => {
+    console.log("clear cart");
+  };
   render() {
     return (
       <ProductContext.Provider
@@ -75,7 +92,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
